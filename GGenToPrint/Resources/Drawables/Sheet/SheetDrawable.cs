6df9d883ba47e6bc44f4bbbe814f3ea498d7dfc0
Ivan <telitsinivan05@yandex.ru>
2023-10-13 10:16:26 +0300

    Add binable properties for graphics

---
+++ GGenToPrint/Resources/Drawables/Sheet/SheetDrawable.cs
@@ -0,0 +1,24 @@
+﻿namespace GGenToPrint.Resources.Drawables.Sheet
+{
+    public class SheetDrawable : IDrawable
+    {
+        public double NumCellsOfVertical { get; set; }
+        public double NumCellsOfHorizontal { get; set; }
+        public double NumCellsOfMargin { get; set; }
+
+        public void Draw(ICanvas canvas, RectF rectF)
+        {
+            AppTheme appTheme = Application.Current.RequestedTheme;
+            canvas.StrokeColor = appTheme == AppTheme.Dark ? Colors.White : Colors.Black;
+
+            canvas.StrokeSize = 1;
+
+            // Border
+            canvas.DrawLine(rectF.Left, rectF.Top, rectF.Right, rectF.Top);
+            canvas.DrawLine(rectF.Right, rectF.Top, rectF.Right, rectF.Bottom);
+            canvas.DrawLine(rectF.Right, rectF.Bottom, rectF.Left, rectF.Bottom);
+            canvas.DrawLine(rectF.Left, rectF.Bottom, rectF.Left, rectF.Top);
+        }
+    }
+}
+
---
+++ GGenToPrint/Resources/Drawables/Sheet/SheetView.cs
@@ -0,0 +1,63 @@
+﻿namespace GGenToPrint.Resources.Drawables.Sheet
+{
+    public class SheetView : GraphicsView
+    {
+        public double NumCellsOfVertical
+        {
+            get => (double)GetValue(NumCellsOfVerticalProperty);
+            set => SetValue(NumCellsOfVerticalProperty, value);
+        }
+        public static readonly BindableProperty NumCellsOfVerticalProperty = BindableProperty.Create(
+            nameof(NumCellsOfVertical), typeof(double), typeof(SheetView),
+            propertyChanged: NumCellsOfVerticalPropertyChanged);
+        public static void NumCellsOfVerticalPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is not SheetView { Drawable: SheetDrawable drawable } view)
+            {
+                return;
+            }
+
+            drawable.NumCellsOfVertical = (double)newValue;
+            view.Invalidate();
+        }
+
+        public double NumCellsOfHorizontal
+        {
+            get => (double)GetValue(NumCellsOfHorizontalProperty);
+            set => SetValue(NumCellsOfHorizontalProperty, value);
+        }
+        public static readonly BindableProperty NumCellsOfHorizontalProperty = BindableProperty.Create(
+            nameof(NumCellsOfHorizontal), typeof(double), typeof(SheetView),
+            propertyChanged: NumCellsOfHorizontalPropertyChanged);
+        public static void NumCellsOfHorizontalPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is not SheetView { Drawable: SheetDrawable drawable } view)
+            {
+                return;
+            }
+
+            drawable.NumCellsOfHorizontal = (double)newValue;
+            view.Invalidate();
+        }
+
+        public double NumCellsOfMargin
+        {
+            get => (double)GetValue(NumCellsOfMarginProperty);
+            set => SetValue(NumCellsOfMarginProperty, value);
+        }
+        public static readonly BindableProperty NumCellsOfMarginProperty = BindableProperty.Create(
+            nameof(NumCellsOfMargin), typeof(double), typeof(SheetView),
+            propertyChanged: NumCellsOfMarginPropertyChanged);
+        public static void NumCellsOfMarginPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is not SheetView { Drawable: SheetDrawable drawable } view)
+            {
+                return;
+            }
+
+            drawable.NumCellsOfMargin = (double)newValue;
+            view.Invalidate();
+        }
+    }
+}
+
