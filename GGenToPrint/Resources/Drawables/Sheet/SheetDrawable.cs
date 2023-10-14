fabd322ede7b9d265090c06dec54b25764b70d43
Ivan <telitsinivan05@ya.ru>
2023-10-15 00:06:37 +0300

    Change sheet view for row type sheet

--- GGenToPrint/Resources/Drawables/Sheet/SheetDrawable.cs
+++ GGenToPrint/Resources/Drawables/Sheet/SheetDrawable.cs
@@ -17,31 +17,16 @@
 
             // Get cell size
             float cellSize = rectF.Width / (float)NumCellsOfHorizontal;
-            if (NumCellsOfVertical * cellSize > rectF.Height || SheetType == 1)
+            if (NumCellsOfVertical * cellSize > rectF.Height)
             {
                 cellSize = rectF.Height / (float)NumCellsOfVertical;
             }
-            float cellHorizontalSize = 0;
-            if (SheetType == 1)
-            {
-                cellHorizontalSize = rectF.Width / (float)NumCellsOfHorizontal;
-            }
 
             // Borders
-            if (SheetType == 0)
-            {
-                canvas.DrawLine(rectF.Left, rectF.Top, cellSize * (float)NumCellsOfHorizontal, rectF.Top);
-                canvas.DrawLine(cellSize * (float)NumCellsOfHorizontal, rectF.Top, cellSize * (float)NumCellsOfHorizontal, cellSize * (float)NumCellsOfVertical);
-                canvas.DrawLine(cellSize * (float)NumCellsOfHorizontal, cellSize * (float)NumCellsOfVertical, rectF.Left, cellSize * (float)NumCellsOfVertical);
-                canvas.DrawLine(rectF.Left, cellSize * (float)NumCellsOfVertical, rectF.Left, rectF.Top);
-            }
-            else if (SheetType == 1)
-            {
-                canvas.DrawLine(rectF.Left, rectF.Top, rectF.Right, rectF.Top);
-                canvas.DrawLine(rectF.Right, rectF.Top, rectF.Right, rectF.Bottom);
-                canvas.DrawLine(rectF.Right, rectF.Bottom, rectF.Left, rectF.Bottom);
-                canvas.DrawLine(rectF.Left, rectF.Bottom, rectF.Left, rectF.Top);
-            }
+            canvas.DrawLine(rectF.Left, rectF.Top, cellSize * (float)NumCellsOfHorizontal, rectF.Top);
+            canvas.DrawLine(cellSize * (float)NumCellsOfHorizontal, rectF.Top, cellSize * (float)NumCellsOfHorizontal, cellSize * (float)NumCellsOfVertical);
+            canvas.DrawLine(cellSize * (float)NumCellsOfHorizontal, cellSize * (float)NumCellsOfVertical, rectF.Left, cellSize * (float)NumCellsOfVertical);
+            canvas.DrawLine(rectF.Left, cellSize * (float)NumCellsOfVertical, rectF.Left, rectF.Top);
 
             // Cells and rows
             if (SheetType == 0)
@@ -59,7 +44,7 @@
             {
                 for (int i = 1; i < NumCellsOfVertical; i++)
                 {
-                    canvas.DrawLine(rectF.Left, cellSize * i, rectF.Right, cellSize * i);
+                    canvas.DrawLine(rectF.Left, cellSize * i, cellSize * (float)NumCellsOfHorizontal, cellSize * i);
                 }
             }
 
@@ -68,43 +53,21 @@
             {
                 canvas.StrokeSize = 3;
                 canvas.StrokeColor = Colors.Red;
-                if (SheetType == 0)
+                if (SheetPosition == 0)
                 {
-                    if (SheetPosition == 0)
-                    {
-                        canvas.DrawLine(
-                            cellSize * (float)NumCellsOfHorizontal - cellSize * (float)MarginSize,
-                            rectF.Top,
-                            cellSize * (float)NumCellsOfHorizontal - cellSize * (float)MarginSize,
-                            cellSize * (float)NumCellsOfVertical);
-                    }
-                    else if (SheetPosition == 1)
-                    {
-                        canvas.DrawLine(
-                            cellSize * (float)MarginSize,
-                            rectF.Top,
-                            cellSize * (float)MarginSize,
-                            cellSize * (float)NumCellsOfVertical);
-                    }
+                    canvas.DrawLine(
+                        cellSize * (float)NumCellsOfHorizontal - cellSize * (float)MarginSize,
+                        rectF.Top,
+                        cellSize * (float)NumCellsOfHorizontal - cellSize * (float)MarginSize,
+                        cellSize * (float)NumCellsOfVertical);
                 }
-                else if (SheetType == 1)
+                else if (SheetPosition == 1)
                 {
-                    if (SheetPosition == 0)
-                    {
-                        canvas.DrawLine(
-                            rectF.Width - cellHorizontalSize * (float)MarginSize,
-                            rectF.Top,
-                            rectF.Width - cellHorizontalSize * (float)MarginSize,
-                            rectF.Bottom);
-                    }
-                    else if (SheetPosition == 1)
-                    {
-                        canvas.DrawLine(
-                            cellHorizontalSize * (float)MarginSize,
-                            rectF.Top,
-                            cellHorizontalSize * (float)MarginSize,
-                            rectF.Bottom);
-                    }
+                    canvas.DrawLine(
+                        cellSize * (float)MarginSize,
+                        rectF.Top,
+                        cellSize * (float)MarginSize,
+                        cellSize * (float)NumCellsOfVertical);
                 }
             }
         }
