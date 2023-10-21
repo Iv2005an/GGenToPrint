c74db1e32f1129ed2ce9aa4114101dd195612c01
Ivan <telitsinivan05@ya.ru>
2023-10-21 23:09:57 +0300

    Added custom control for option with slider

---
+++ GGenToPrint/Resources/Controls/SliderOption.cs
@@ -0,0 +1,34 @@
+﻿namespace GGenToPrint.Resources.Controls;
+
+public class SliderOption : ContentView
+{
+    public static readonly BindableProperty OptionNameProperty = BindableProperty.Create(nameof(OptionName), typeof(string), typeof(SliderOption));
+    public string OptionName
+    {
+
+        get => (string)GetValue(OptionNameProperty);
+        set => SetValue(OptionNameProperty, value);
+    }
+
+    public static readonly BindableProperty SliderMinimumProperty = BindableProperty.Create(nameof(SliderMinimum), typeof(byte), typeof(SliderOption));
+    public byte SliderMinimum
+    {
+
+        get => (byte)GetValue(SliderMinimumProperty);
+        set => SetValue(SliderMinimumProperty, value);
+    }
+    public static readonly BindableProperty SliderMaximumProperty = BindableProperty.Create(nameof(SliderMaximum), typeof(byte), typeof(SliderOption));
+    public byte SliderMaximum
+    {
+
+        get => (byte)GetValue(SliderMaximumProperty);
+        set => SetValue(SliderMaximumProperty, value);
+    }
+    public static readonly BindableProperty SliderValueProperty = BindableProperty.Create(nameof(SliderValue), typeof(byte), typeof(SliderOption), defaultBindingMode: BindingMode.TwoWay);
+    public byte SliderValue
+    {
+
+        get => (byte)GetValue(SliderValueProperty);
+        set => SetValue(SliderValueProperty, value);
+    }
+}
