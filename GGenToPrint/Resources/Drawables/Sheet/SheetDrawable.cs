e102fd38735f9b649005456d013952cf612789fe
Ivan <telitsinivan05@ya.ru>
2023-10-22 22:30:13 +0300

    Added centering of the sheet

--- GGenToPrint/Resources/Drawables/Sheet/SheetDrawable.cs
+++ GGenToPrint/Resources/Drawables/Sheet/SheetDrawable.cs
@@ -1,77 +1,82 @@
-﻿namespace GGenToPrint.Resources.Drawables.Sheet
+﻿namespace GGenToPrint.Resources.Drawables.Sheet;
+
+public class SheetDrawable : IDrawable
 {
-    public class SheetDrawable : IDrawable
+    public byte NumCellsOfVertical { get; set; }
+    public byte NumCellsOfHorizontal { get; set; }
+    public byte NumCellsOfMargin { get; set; }
+    public byte SheetTypeIndex { get; set; }
+    public byte SheetPositionIndex { get; set; }
+
+    public void Draw(ICanvas canvas, RectF rectF)
     {
-        public byte NumCellsOfVertical { get; set; }
-        public byte NumCellsOfHorizontal { get; set; }
-        public byte NumCellsOfMargin { get; set; }
-        public byte SheetTypeIndex { get; set; }
-        public byte SheetPositionIndex { get; set; }
+        // Get color for paint
+        AppTheme appTheme = Application.Current.RequestedTheme;
+        Color drawColor = appTheme == AppTheme.Dark ? Colors.White : Colors.Black;
+        canvas.StrokeColor = drawColor;
 
-        public void Draw(ICanvas canvas, RectF rectF)
+        // Get cell size
+        float cellSize = rectF.Width / NumCellsOfHorizontal;
+        if (NumCellsOfVertical * cellSize > rectF.Height)
         {
-            // Get color for paint
-            AppTheme appTheme = Application.Current.RequestedTheme;
-            Color drawColor = appTheme == AppTheme.Dark ? Colors.White : Colors.Black;
+            cellSize = rectF.Height / NumCellsOfVertical;
+        }
+
+        // Get draw position
+        var top = rectF.Height / 2 - cellSize * NumCellsOfVertical / 2;
+        var bottom = top + cellSize * NumCellsOfVertical;
+        var left = rectF.Width / 2 - cellSize * NumCellsOfHorizontal / 2;
+        var right = left + cellSize * NumCellsOfHorizontal;
+
+        // Borders
+        canvas.DrawLine(left, top, right, top);
+        canvas.DrawLine(right, top, right, bottom);
+        canvas.DrawLine(right, bottom, left, bottom);
+        canvas.DrawLine(left, bottom, left, top);
+
+        // Cells and rows
+        if (SheetTypeIndex == 0)
+        {
+            for (byte i = 1; i < NumCellsOfHorizontal; i++)
+            {
+                canvas.DrawLine(left + cellSize * i, top, left + cellSize * i, bottom);
+            }
+            for (byte i = 1; i < NumCellsOfVertical; i++)
+            {
+                canvas.DrawLine(left, top + cellSize * i, right, top + cellSize * i);
+            }
+        }
+        else if (SheetTypeIndex == 1)
+        {
+            for (byte i = 1; i < NumCellsOfVertical; i++)
+            {
+                canvas.DrawLine(left, top + cellSize * i, right, top + cellSize * i);
+            }
+        }
+
+        // Margin
+        if (NumCellsOfMargin > 0)
+        {
+            canvas.StrokeSize = 3;
+            canvas.StrokeColor = Colors.Red;
+            if (SheetPositionIndex == 0)
+            {
+                canvas.DrawLine(
+                    right - cellSize * NumCellsOfMargin,
+                    top,
+                    right - cellSize * NumCellsOfMargin,
+                    bottom);
+            }
+            else if (SheetPositionIndex == 1)
+            {
+                canvas.DrawLine(
+                    cellSize * NumCellsOfMargin,
+                    top,
+                    cellSize * NumCellsOfMargin,
+                    bottom);
+            }
+            canvas.StrokeSize = 1;
             canvas.StrokeColor = drawColor;
-
-            // Get cell size
-            float cellSize = rectF.Width / NumCellsOfHorizontal;
-            if (NumCellsOfVertical * cellSize > rectF.Height)
-            {
-                cellSize = rectF.Height / NumCellsOfVertical;
-            }
-
-            // Borders
-            canvas.DrawLine(rectF.Left, rectF.Top, cellSize * NumCellsOfHorizontal, rectF.Top);
-            canvas.DrawLine(cellSize * NumCellsOfHorizontal, rectF.Top, cellSize * NumCellsOfHorizontal, cellSize * NumCellsOfVertical);
-            canvas.DrawLine(cellSize * NumCellsOfHorizontal, cellSize * NumCellsOfVertical, rectF.Left, cellSize * NumCellsOfVertical);
-            canvas.DrawLine(rectF.Left, cellSize * NumCellsOfVertical, rectF.Left, rectF.Top);
-
-            // Cells and rows
-            if (SheetTypeIndex == 0)
-            {
-                for (byte i = 1; i < NumCellsOfHorizontal; i++)
-                {
-                    canvas.DrawLine(cellSize * i, rectF.Top, cellSize * i, cellSize * NumCellsOfVertical);
-                }
-                for (byte i = 1; i < NumCellsOfVertical; i++)
-                {
-                    canvas.DrawLine(rectF.Left, cellSize * i, cellSize * NumCellsOfHorizontal, cellSize * i);
-                }
-            }
-            else if (SheetTypeIndex == 1)
-            {
-                for (byte i = 1; i < NumCellsOfVertical; i++)
-                {
-                    canvas.DrawLine(rectF.Left, cellSize * i, cellSize * NumCellsOfHorizontal, cellSize * i);
-                }
-            }
-
-            // Margin
-            if (NumCellsOfMargin > 0)
-            {
-                canvas.StrokeSize = 3;
-                canvas.StrokeColor = Colors.Red;
-                if (SheetPositionIndex == 0)
-                {
-                    canvas.DrawLine(
-                        cellSize * NumCellsOfHorizontal - cellSize * NumCellsOfMargin,
-                        rectF.Top,
-                        cellSize * NumCellsOfHorizontal - cellSize * NumCellsOfMargin,
-                        cellSize * NumCellsOfVertical);
-                }
-                else if (SheetPositionIndex == 1)
-                {
-                    canvas.DrawLine(
-                        cellSize * NumCellsOfMargin,
-                        rectF.Top,
-                        cellSize * NumCellsOfMargin,
-                        cellSize * NumCellsOfVertical);
-                }
-                canvas.StrokeSize = 1;
-                canvas.StrokeColor = drawColor;
-            }
         }
     }
 }
--- GGenToPrint/Resources/Drawables/Sheet/SheetView.cs
+++ GGenToPrint/Resources/Drawables/Sheet/SheetView.cs
@@ -1,100 +1,99 @@
-﻿namespace GGenToPrint.Resources.Drawables.Sheet
+﻿namespace GGenToPrint.Resources.Drawables.Sheet;
+
+public class SheetView : GraphicsView
 {
-    public class SheetView : GraphicsView
+    public byte NumCellsOfVertical
     {
-        public byte NumCellsOfVertical
+        get => (byte)GetValue(NumCellsOfVerticalProperty);
+        set => SetValue(NumCellsOfVerticalProperty, value);
+    }
+    public static readonly BindableProperty NumCellsOfVerticalProperty = BindableProperty.Create(
+        nameof(NumCellsOfVertical), typeof(byte), typeof(SheetView),
+        propertyChanged: NumCellsOfVerticalPropertyChanged);
+    public static void NumCellsOfVerticalPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is not SheetView { Drawable: SheetDrawable sheetDrawable } sheetView)
         {
-            get => (byte)GetValue(NumCellsOfVerticalProperty);
-            set => SetValue(NumCellsOfVerticalProperty, value);
-        }
-        public static readonly BindableProperty NumCellsOfVerticalProperty = BindableProperty.Create(
-            nameof(NumCellsOfVertical), typeof(byte), typeof(SheetView),
-            propertyChanged: NumCellsOfVerticalPropertyChanged);
-        public static void NumCellsOfVerticalPropertyChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            if (bindable is not SheetView { Drawable: SheetDrawable sheetDrawable } sheetView)
-            {
-                return;
-            }
-
-            sheetDrawable.NumCellsOfVertical = (byte)newValue;
-            sheetView.Invalidate();
+            return;
         }
 
-        public byte NumCellsOfHorizontal
-        {
-            get => (byte)GetValue(NumCellsOfHorizontalProperty);
-            set => SetValue(NumCellsOfHorizontalProperty, value);
-        }
-        public static readonly BindableProperty NumCellsOfHorizontalProperty = BindableProperty.Create(
-            nameof(NumCellsOfHorizontal), typeof(byte), typeof(SheetView),
-            propertyChanged: NumCellsOfHorizontalPropertyChanged);
-        public static void NumCellsOfHorizontalPropertyChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            if (bindable is not SheetView { Drawable: SheetDrawable sheetDrawable } sheetView)
-            {
-                return;
-            }
+        sheetDrawable.NumCellsOfVertical = (byte)newValue;
+        sheetView.Invalidate();
+    }
 
-            sheetDrawable.NumCellsOfHorizontal = (byte)newValue;
-            sheetView.Invalidate();
-        }
-
-        public byte NumCellsOfMargin
+    public byte NumCellsOfHorizontal
+    {
+        get => (byte)GetValue(NumCellsOfHorizontalProperty);
+        set => SetValue(NumCellsOfHorizontalProperty, value);
+    }
+    public static readonly BindableProperty NumCellsOfHorizontalProperty = BindableProperty.Create(
+        nameof(NumCellsOfHorizontal), typeof(byte), typeof(SheetView),
+        propertyChanged: NumCellsOfHorizontalPropertyChanged);
+    public static void NumCellsOfHorizontalPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is not SheetView { Drawable: SheetDrawable sheetDrawable } sheetView)
         {
-            get => (byte)GetValue(NumCellsOfMarginProperty);
-            set => SetValue(NumCellsOfMarginProperty, value);
-        }
-        public static readonly BindableProperty NumCellsOfMarginProperty = BindableProperty.Create(
-            nameof(NumCellsOfMargin), typeof(byte), typeof(SheetView),
-            propertyChanged: NumCellsOfMarginPropertyChanged);
-        public static void NumCellsOfMarginPropertyChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            if (bindable is not SheetView { Drawable: SheetDrawable sheetDrawable } sheetView)
-            {
-                return;
-            }
-
-            sheetDrawable.NumCellsOfMargin = (byte)newValue;
-            sheetView.Invalidate();
+            return;
         }
 
-        public byte SheetTypeIndex
-        {
-            get => (byte)GetValue(SheetTypeIndexProperty);
-            set => SetValue(SheetTypeIndexProperty, value);
-        }
-        public static readonly BindableProperty SheetTypeIndexProperty = BindableProperty.Create(
-            nameof(SheetTypeIndex), typeof(byte), typeof(SheetView),
-            propertyChanged: SheetTypePropertyChanged);
-        public static void SheetTypePropertyChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            if (bindable is not SheetView { Drawable: SheetDrawable sheetDrawable } sheetView)
-            {
-                return;
-            }
+        sheetDrawable.NumCellsOfHorizontal = (byte)newValue;
+        sheetView.Invalidate();
+    }
 
-            sheetDrawable.SheetTypeIndex = (byte)newValue;
-            sheetView.Invalidate();
+    public byte NumCellsOfMargin
+    {
+        get => (byte)GetValue(NumCellsOfMarginProperty);
+        set => SetValue(NumCellsOfMarginProperty, value);
+    }
+    public static readonly BindableProperty NumCellsOfMarginProperty = BindableProperty.Create(
+        nameof(NumCellsOfMargin), typeof(byte), typeof(SheetView),
+        propertyChanged: NumCellsOfMarginPropertyChanged);
+    public static void NumCellsOfMarginPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is not SheetView { Drawable: SheetDrawable sheetDrawable } sheetView)
+        {
+            return;
         }
 
-        public byte SheetPositionIndex
-        {
-            get => (byte)GetValue(SheetPositionIndexProperty);
-            set => SetValue(SheetPositionIndexProperty, value);
-        }
-        public static readonly BindableProperty SheetPositionIndexProperty = BindableProperty.Create(
-            nameof(SheetPositionIndex), typeof(byte), typeof(SheetView),
-            propertyChanged: SheetPositionIndexPropertyChanged);
-        public static void SheetPositionIndexPropertyChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            if (bindable is not SheetView { Drawable: SheetDrawable sheetDrawable } sheetView)
-            {
-                return;
-            }
+        sheetDrawable.NumCellsOfMargin = (byte)newValue;
+        sheetView.Invalidate();
+    }
 
-            sheetDrawable.SheetPositionIndex = (byte)newValue;
-            sheetView.Invalidate();
+    public byte SheetTypeIndex
+    {
+        get => (byte)GetValue(SheetTypeIndexProperty);
+        set => SetValue(SheetTypeIndexProperty, value);
+    }
+    public static readonly BindableProperty SheetTypeIndexProperty = BindableProperty.Create(
+        nameof(SheetTypeIndex), typeof(byte), typeof(SheetView),
+        propertyChanged: SheetTypePropertyChanged);
+    public static void SheetTypePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is not SheetView { Drawable: SheetDrawable sheetDrawable } sheetView)
+        {
+            return;
         }
+
+        sheetDrawable.SheetTypeIndex = (byte)newValue;
+        sheetView.Invalidate();
+    }
+
+    public byte SheetPositionIndex
+    {
+        get => (byte)GetValue(SheetPositionIndexProperty);
+        set => SetValue(SheetPositionIndexProperty, value);
+    }
+    public static readonly BindableProperty SheetPositionIndexProperty = BindableProperty.Create(
+        nameof(SheetPositionIndex), typeof(byte), typeof(SheetView),
+        propertyChanged: SheetPositionIndexPropertyChanged);
+    public static void SheetPositionIndexPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is not SheetView { Drawable: SheetDrawable sheetDrawable } sheetView)
+        {
+            return;
+        }
+
+        sheetDrawable.SheetPositionIndex = (byte)newValue;
+        sheetView.Invalidate();
     }
 }
