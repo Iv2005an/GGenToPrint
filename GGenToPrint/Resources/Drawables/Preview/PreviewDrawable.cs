e09d9f135225796a4758907fee818669d274bccc
Ivan <telitsinivan05@ya.ru>
2023-10-30 00:03:01 +0300

    Added  drawing character preview from commands

--- GGenToPrint/Resources/Drawables/Preview/PreviewDrawable.cs
+++ GGenToPrint/Resources/Drawables/Preview/PreviewDrawable.cs
@@ -1,4 +1,6 @@
-﻿namespace GGenToPrint.Resources.Drawables.Preview;
+﻿using GGenToPrint.Resources.Models;
+
+namespace GGenToPrint.Resources.Drawables.Preview;
 
 public class PreviewDrawable : IDrawable
 {
@@ -6,10 +8,24 @@ public class PreviewDrawable : IDrawable
 
     public void Draw(ICanvas canvas, RectF rectF)
     {
+#if DEBUG
+        Commands =
+            "G0 X2 Y1\n" +
+            "G1 X2 Y4\n" +
+            "G1 X3 Y5\n" +
+            "G1 X4 Y4\n" +
+            "G1 X4 Y1\n" +
+            "G1 X4 Y5\n" +
+            "G0 X2 Y0\n" +
+            "G1 X3 Y1\n" +
+            "G1 X4 Y0\n" +
+            "G0 X4 Y5";
+#endif
         // Get color for paint
         AppTheme appTheme = Application.Current.RequestedTheme;
         Color drawColor = appTheme == AppTheme.Dark ? Colors.White : Colors.Black;
         canvas.StrokeColor = drawColor;
+        canvas.FontColor = drawColor;
 
         // Get draw position
         var smallerSide = rectF.Width < rectF.Height ? rectF.Width : rectF.Height;
@@ -25,7 +41,6 @@ public class PreviewDrawable : IDrawable
         canvas.DrawLine(right, top, right, bottom);
         canvas.DrawLine(right, bottom, left, bottom);
         canvas.DrawLine(left, bottom, left, top);
-        canvas.DrawString(Commands, rectF.Width / 2, rectF.Height / 2, HorizontalAlignment.Center);
 
         // Cells
         for (byte i = 1; i <= 6; i++)
@@ -33,5 +48,32 @@ public class PreviewDrawable : IDrawable
             canvas.DrawLine(left, top + cellSize * i, right, top + cellSize * i);
             canvas.DrawLine(left + cellSize * i, top, left + cellSize * i, bottom);
         }
+        canvas.StrokeColor = Colors.LightBlue;
+        canvas.DrawLine(left, top + cellSize * 3, right, top + cellSize * 3);
+
+        //Draw character
+        canvas.StrokeColor = Colors.Blue;
+        if (Commands != null)
+        {
+            Gcommand lastCommand = null;
+            foreach (var command in Gcommand.ParseCommands(Commands))
+            {
+                if (lastCommand is null || command.Gcode == "G0")
+                {
+                    lastCommand = command;
+                }
+                else
+                {
+                    canvas.DrawLine(
+                        left + lastCommand.XCoordinate * cellSize,
+                        top + lastCommand.YCoordinate * cellSize,
+                        left + command.XCoordinate * cellSize,
+                        top + command.YCoordinate * cellSize);
+                    lastCommand = command;
+
+                }
+            }
+        }
+        canvas.StrokeColor = drawColor;
     }
 }
---
+++ GGenToPrint/Resources/Models/Gcommand.cs
@@ -0,0 +1,24 @@
+﻿namespace GGenToPrint.Resources.Models;
+
+public class Gcommand
+{
+    public static IEnumerable<Gcommand> ParseCommands(string commands)
+    {
+        var Commands = new List<Gcommand>();
+        foreach (string command in commands.Split('\n'))
+        {
+            string[] args = command.Split();
+            Commands.Add(new Gcommand()
+            {
+                Gcode = args[0],
+                XCoordinate = (float)Convert.ToDouble(args[1][1..]),
+                YCoordinate = (float)Convert.ToDouble(args[2][1..])
+            });
+        }
+        return Commands;
+    }
+
+    public string Gcode { get; set; }
+    public float XCoordinate { get; set; }
+    public float YCoordinate { get; set; }
+}
