883e2dec20e52ccc2e11b757f5e9557d504baf3c
Ivan <telitsinivan05@ya.ru>
2023-10-31 17:46:19 +0300

    Fixed duplication of commands

--- GGenToPrint/Resources/ViewModels/EditPageViewModel.cs
+++ GGenToPrint/Resources/ViewModels/EditPageViewModel.cs
@@ -22,13 +22,28 @@ public partial class EditPageViewModel : ObservableObject
     [ObservableProperty]
     float cellSize;
 
+    bool IsEqualCommands(PointF newCoordinates)
+    {
+        bool equalsCommands = false;
+        if (Commands != null)
+        {
+            var last_command = Gcommand.ParseCommands(Commands).LastOrDefault();
+            var last_x = last_command.XCoordinate;
+            var last_y = last_command.YCoordinate;
+            var new_x = (float)Math.Round(newCoordinates.X / CellSize, 2);
+            var new_y = (float)Math.Round(newCoordinates.Y / CellSize, 2);
+            equalsCommands = last_x == new_x && last_y == new_y;
+        }
+        return equalsCommands;
+    }
     bool outOfBorders;
     [RelayCommand]
     void StartCommandsChanging(PointF coordinates)
     {
         if (coordinates.X < CellSize * 6 && coordinates.Y < CellSize * 6)
         {
-            Commands += $"G0 X{coordinates.X / CellSize:0.00} Y{coordinates.Y / CellSize:0.00}\n";
+            if (!IsEqualCommands(coordinates))
+                Commands += $"G0 X{coordinates.X / CellSize:0.00} Y{coordinates.Y / CellSize:0.00}\n";
             outOfBorders = false;
         }
     }
@@ -37,7 +52,10 @@ public partial class EditPageViewModel : ObservableObject
     void CommandsChanging(PointF coordinates)
     {
         if (!outOfBorders && coordinates.X < CellSize * 6 && coordinates.Y < CellSize * 6)
-            Commands += $"G1 X{coordinates.X / CellSize:0.00} Y{coordinates.Y / CellSize:0.00}\n";
+        {
+            if (!IsEqualCommands(coordinates))
+                Commands += $"G1 X{coordinates.X / CellSize:0.00} Y{coordinates.Y / CellSize:0.00}\n";
+        }
         else
             outOfBorders = true;
     }
